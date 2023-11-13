badf036c8ea70b5abbc2f08a295eda7fd7ab6bb2
Mihael Krpes <mikrpes@vsite.hr>
2023-11-13 21:15:57 +0100

    Add project files.

---
+++ MVCCar/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCCar.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }=string.Empty;
+        public string Manufacturer { get; set; }= string.Empty;
+        public string Color { get; set; }=string.Empty;
+        public int ProductionYear { get; set; }
+        public double Price { get; set; }
+    }
+}
